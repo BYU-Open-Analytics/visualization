f91d28c3ead07a24265f5d860fc4c89277359825
Robert Nay <rsn.nay@gmail.com>
2015-08-06 11:21:51 -0600

    Friendlier LTI launch page

--- public/launch.php
+++ public/launch.php
@@ -5,7 +5,6 @@ require_once '../app/library/ims_lti/blti.php';
 
 //Get lti key and secret from app's config file
 $config = require('../app/config/config.php');
-print_r($config);
 $context = new BLTI($config["lti"]["launch"], true, true);
 if ( $context->valid ) {
 	Header("Location: ./");
@@ -13,32 +12,32 @@ if ( $context->valid ) {
 ?>
 <html>
 <head>
-  <title>IMS Basic Learning Tools Interoperability</title>
+  <title>Dashboard LTI Launch</title>
 </head>
 <body style="font-family:sans-serif">
-<p><b>IMS BasicLTI PHP Provider</b></p>
-<p>This is a very simple Basic LTI Tool.  If the message is a Basic LTI Launch,
-it checks the signature and if the signature is OK,  it establishes context.
-All secrets are "secret".
-</p>
+<h2>Dashboard LTI Launch</h2>
 <?php
 
 if ( $context->valid ) {
     //Dumps all info (post params are stored in here for later use)
     //print_r($context->info);
-    print "<pre>\n";
-    print "Context Information:\n\n";
-    print $context->dump();
-    print "</pre>\n";
+    if (isset($_GET["debug"])) {
+	    print "<pre>\n";
+	    print "Context Information:\n\n";
+	    print $context->dump();
+	    print "</pre>\n";
+    }
 } else {
-    print "<p style=\"color:red\">Could not establish context: ".$context->message."<p>\n";
+    print "<p style=\"color:red\">Could not establish context: ".$context->message.". Try launching this app again.<p>\n";
 }
 
-print "<pre>\n";
-print "Raw POST Parameters:\n\n";
-foreach($_POST as $key => $value ) {
-    print "$key=$value\n";
+if (isset($_GET["debug"])) {
+	print "<pre>\n";
+	print "Raw POST Parameters:\n\n";
+	foreach($_POST as $key => $value ) {
+	    print "$key=$value\n";
+	}
+	print "</pre>";
 }
-print "</pre>";
 
 ?>
