6c16cd2535ed70517c778c85ba85c7ff8b29390a
Robert Nay <rsn.nay@gmail.com>
2015-08-06 12:14:24 -0600

    Backend function for confidence averages

--- app/controllers/AssessmentStatsController.php
+++ app/controllers/AssessmentStatsController.php
@@ -98,20 +98,31 @@ class AssessmentStatsController extends Controller
 		$result = Array();
 		$statementHelper = new StatementHelper();
 
+		// Numerical confidence values
+		$levelValue = ["low" => -1, "medium" => 0, "high" => 1];
+
+		$confidences = ["overall" => array(), "correct" => array(), "incorrect" => array()];
+
 		//Get all user answer attempts
-		$attempts = $statementHelper->getStatements("openassessments",'[{"$match":{"voided":false, "statement.actor.mbox": "mailto:'.$context->getUserEmail().'", "statement.verb.id":"http://adlnet.gov/expapi/verbs/answered"}},{"$project":{"_id":0, "statement.context.extensions":1 }}]');
+		$attempts = $statementHelper->getStatements("openassessments",'[{"$match":{"voided":false, "statement.actor.mbox": "mailto:'.$context->getUserEmail().'", "statement.verb.id":"http://adlnet.gov/expapi/verbs/answered"}},{"$project":{"_id":0, "statement.context.extensions":1, "statement.result.success":1 }}]');
 		if ($attempts["error"]) {
 			$result []= ["error" => $attempts["error"]];
 		} else {
 			foreach ($attempts["statements"] as $statement) {
-				if (isset($statement->statement->context->extensions->{"http://byuopenanalytics.byu.edu/expapi/extensions/confidence_level"})) {
+				if (isset($statement->statement->context->extensions->{"http://byuopenanalytics.byu.edu/expapi/extensions/confidence_level"}) && isset($statement->statement->result->success)) {
 					$level = $statement->statement->context->extensions->{"http://byuopenanalytics.byu.edu/expapi/extensions/confidence_level"};
-					if (isset($result[$level])) {
-						$result[$level] += 1;
+					$correct = $statement->statement->result->success;
+					$confidences["overall"] []= $levelValue[$level];
+					if ($correct == true) {
+						$confidences["correct"] []= $levelValue[$level];
 					} else {
-						$result[$level] = 1;
+						$confidences["incorrect"] []= $levelValue[$level];
 					}
 				}
+			}
+			foreach ($confidences as $name => $list) {
+				$average = array_sum($list) / count($list);
+				$result []= ["name" => $name, "value" => $average];
 			}
 			echo json_encode($result);
 		}
