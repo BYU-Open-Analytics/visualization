bdf5019f821555f2ba25252d3974508e1ab0e74f
Robert Nay <rsn.nay@gmail.com>
2015-08-07 13:06:34 -0600

    Switch Ayamel stats to mongo

--- app/controllers/AyamelStatsController.php
+++ app/controllers/AyamelStatsController.php
@@ -22,30 +22,28 @@ class AyamelStatsController extends Controller
 		$result = Array();
 		$statementHelper = new StatementHelper();
 
-		// This contains our different data elements
-		$result = Array();
-		$statementHelper = new StatementHelper();
-
 		$verbs = Array();
 
 		//Get verbs of all statements
 		//$statements = $statementHelper->getStatements("ayamel",'[{"$match":{"voided":false}},{"$project":{"statement.verb.display.en-US":1}}]');
-		$statements = $statementHelper->getStatements("ayamel",['voided'=>false], ['_id'=>false, 'statement.verb.display.en-US'=>true]);
+		$statements = $statementHelper->getStatements("ayamel",[], ['_id'=>false, 'statement.verb.display.en-US'=>true]);
+
+
+
 		if ($statements["error"]) {
 			$result []= ["error" => $attempts["error"]];
 		} else {
 			//Count how many we have of each verb
-			foreach ($statements["statements"] as $statement) {
-				$verb = $statement->statement->verb->display->{"en-US"};
+			foreach ($statements["cursor"] as $statement) {
+				$verb = $statement['statement']['verb']['display']['en-US'];
 				if (isset($verbs[$verb])) {
 					$verbs[$verb] += 1;
 				} else {
 					$verbs[$verb] = 1;
 				}
-				//echo($statement->statement->verb->display->{"en-US"}."<br>");
 			}
 			//Only include if it has at least 3% of total statements
-			$inclusionThreshold = count($statements["statements"]) * 0.03;
+			$inclusionThreshold = $statements["cursor"]->count() * 0.03;
 			//Put the verb counts into the format we want in d3
 			foreach ($verbs as $verb => $count) {
 				if ($count > $inclusionThreshold) {
--- app/helpers/StatementHelper.php
+++ app/helpers/StatementHelper.php
@@ -18,47 +18,18 @@ class StatementHelper extends Module {
 
 		// Query statements
 		$collection = $db->statements;
-		//From PHP mongo docs: Please make sure that for all special query operators (starting with $) you use single quotes so that PHP doesn't try to replace "$exists" with the value of the variable $exists.
+		// Use $lrs to support pulling statements from specific LRSs (ayamel, open assessments, visualization settings)
 		if (!empty($lrs)) {
+			// TODO create mongodb index for this
 			$query["lrs._id"] = $config->lrs->{$lrs}->id;
 		}
 
+		//From PHP mongo docs: Please make sure that for all special query operators (starting with $) you use single quotes so that PHP doesn't try to replace "$exists" with the value of the variable $exists.
 		$cursor = $collection->find($query, $fields);
 
-
-		// Can't just include entire statement ("statement":1 in $project block), or learning locker php script will run out of memory
-
-		// Use $lrs to support pulling statements from different LRSs (ayamel, open assessments, visualization settings)
-		$request = $config->lrs->{$lrs}->endpoint.'api/v1/statements/aggregate?pipeline='.$pipeline;
-		$session = curl_init($request);
-		curl_setopt($session, CURLOPT_USERPWD, $config->lrs->{$lrs}->username . ":" . $config->lrs->{$lrs}->password);
-		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-
-		$response = curl_exec($session);
-
-		// Catch curl errors
-		if (curl_errno($session)) {
-			$error = "Curl error: " . curl_error($session);
-		}
-		$statusCode = curl_getinfo($session, CURLINFO_HTTP_CODE);
-		$error .= $statusCode;
-
-		curl_close($session);
-
-
-		$parsed = json_decode($response);
-		if (isset($parsed->ok) && $parsed->ok == 1) {
-			if (isset($parsed->result) && $statements = $parsed->result) {
-				$error = null;
-			} else {
-				$error .= " Didn't receive any statements from Learning Locker with request of $request";
-			}
-		} elseif (isset($parsed->error) && $parsed->error == true) {
-			$error .= " LL error: {$parsed->message}";
-		} else {
-			$error .= " Unknown error in Learning Locker response";
-		}
-		return ["error"=>$error, "statements"=>$statements];
+		// TODO error handling
+		//$error .= " Didn't receive any statements from Learning Locker with request of $request";
+		return ["error"=>$error, "cursor"=>$cursor];
 	}
 
 }
