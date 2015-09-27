38fa39379a742a1979daf6e3590c0e818b412f7e
Robert Nay <rsn.nay@gmail.com>
2015-09-27 00:48:38 +0000

    Make concept question attempt count a bit more efficient

--- app/helpers/MasteryHelper.php
+++ app/helpers/MasteryHelper.php
@@ -9,19 +9,16 @@ class MasteryHelper extends Module {
 		$statementHelper = new StatementHelper();
 
 		// Get questions in concept
-		// TODO remove hardcoded example question IDs
 		$questionIds = array();
-		
-
 		$conceptQuestions = CSVHelper::parseWithHeaders('csv/concept_question.csv');
 		// Filter questions to ones in the selected concept
 		$questionLists = array_filter($conceptQuestions, function($concept) use ($conceptId) {
 			return ($concept["concept_number"] == $conceptId);
 		});
+		// Combine multiple rows of questions that are with the same concept
 		array_walk($questionLists, function($concept) use (&$questionIds) {
 			$questionIds = array_merge($questionIds, explode(",", $concept["questions"]));
 		});
-			
 		//echo "Concept ID $conceptId: ";
 		//print_r($questionIds);
 		//echo "<hr>";
@@ -30,33 +27,39 @@ class MasteryHelper extends Module {
 		// Calculate total # attempts (answered statements) for all questions in the concept
 		$conceptTotalAttempts = 0;
 
+		// Load quiz id -> assessment id mapping
+		$assessmentIds = CSVHelper::parseWithHeaders('csv/quiz_assessmentid.csv');
+
 		// Loop through each question
 		foreach ($questionIds as $questionId) {
-			// Split up quiz id and question id from format 12.1 (quizId.questionNumber)
+			// Split up quiz id and question id from format 12.1 (quizNumber.questionNumber)
 			$idParts = explode(".", $questionId);
 			// Make sure we have a (at least format-wise) valid question id
 			if (count($idParts) != 2) {
 				continue;
 			}
-			$quizId = $idParts[0];
+			$quizNumber = $idParts[0];
 			$questionNumber = $idParts[1];
 			// Get assessment id from quiz id
-			// TODO actually do this
-			$assessmentId = $quizId;
+			$assessmentId = $assessmentIds[array_search($quizNumber, array_column($assessmentIds, 'quiz_number'))]["assessment_id"];
 
 			// Regex for assessment id and question number (since url for object IDs will change, but end part will be the same format)
 			$regex = new MongoRegex('/' . $assessmentId . '\.xml#' . $questionNumber . '$/');
+			$questionDescription = "Question #{$questionNumber} of assessment {$assessmentId}";
 			// Get the count of answered statements for this question for current user
 			$statements = $statementHelper->getStatements("openassessments",[
 				'statement.actor.mbox' => 'mailto:'.$studentId,
 				'statement.verb.id' => 'http://adlnet.gov/expapi/verbs/answered',
-				'statement.object.id' => $regex,
+				//'statement.object.id' => $regex,
+				'statement.object.definition.name.en-US' => $questionDescription,
 			], [
 				'statement.result.success' => true,
 			]);
 			if ($statements["error"]) {
 				// TODO error handling
 			} else {
+				// My attempt at debugging slow queries to figure out how to speed them out
+				//var_dump($statements["cursor"]->explain());
 				$conceptTotalAttempts += $statements["cursor"]->count();
 			}
 		}
