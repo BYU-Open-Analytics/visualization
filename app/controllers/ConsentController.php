b4f301dc17f1c6dc162a7bf188b2eab346e621a1
Robert Nay <rsn.nay@gmail.com>
2015-10-15 22:36:26 +0000

    Add consent page

--- app/controllers/ConsentController.php
+++ app/controllers/ConsentController.php
@@ -2,16 +2,16 @@
 
 use Phalcon\Mvc\Controller;
 
-class IndexController extends Controller
+class ConsentController extends Controller
 {
 	public function initialize() {
-		$this->tag->setTitle('Home');
+		$this->tag->setTitle('Consent Required');
 	}
 	public function indexAction() {
 		// Get our context (this takes care of starting the session, too)
 		$context = $this->getDI()->getShared('ltiContext');
 		$this->view->ltiContext = $context;
 		$this->view->userAuthorized = $context->valid;
-		// Figure out where this student should go: Content Recommender, Student Skills, or Consent page
+		$this->view->consentEmail = $this->getDI()->getShared('config')->consent_email;
 	}
 }
