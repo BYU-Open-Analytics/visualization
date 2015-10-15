8ee7d00c3cfc5efb645d1f4912efc0f200fcdfb4
Robert Nay <rsn.nay@gmail.com>
2015-10-15 23:37:02 +0000

    Control info page date

--- app/controllers/InfoController.php
+++ app/controllers/InfoController.php
@@ -4,10 +4,8 @@ use Phalcon\Mvc\Controller;
 
 class InfoController extends Controller
 {
-	public function initialize() {
-		$this->tag->setTitle('Consent Required');
-	}
 	public function controlAction() {
+		$this->tag->setTitle('Consent Required');
 		// Get our context (this takes care of starting the session, too)
 		$context = $this->getDI()->getShared('ltiContext');
 		$this->view->ltiContext = $context;
@@ -15,6 +13,7 @@ class InfoController extends Controller
 		$this->view->consentEmail = $this->getDI()->getShared('config')->consent_email;
 	}
 	public function consentAction() {
+		$this->tag->setTitle('Check Back Later');
 		// Get our context (this takes care of starting the session, too)
 		$context = $this->getDI()->getShared('ltiContext');
 		$this->view->ltiContext = $context;
