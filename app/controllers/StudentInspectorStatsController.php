88b555e9b635723472b219a301f2abda39c89a1a
Robert Nay <rsn.nay@gmail.com>
2016-01-08 20:29:07 +0000

    Basic d3 table and student inpsector stats stuff

--- app/controllers/StudentInspectorStatsController.php
+++ app/controllers/StudentInspectorStatsController.php
@@ -9,6 +9,33 @@ class StudentInspectorStatsController extends Controller
 		$this->tag->setTitle('Assessment Stats');
 	}
 
+	public function studentsAction($scope = 'unit', $groupingId = '3', $debug = false) {
+		$this->view->disable();
+		// Get our context (this takes care of starting the session, too)
+		$context = $this->getDI()->getShared('ltiContext');
+		if (!$context->valid) {
+			echo '[{"error":"Invalid lti context"}]';
+			return;
+		}
+		$classHelper = new ClassHelper();
+		$masteryHelper = new MasteryHelper();
+		$statementHelper = new StatementHelper();
+		$students = $classHelper->allStudents();
+		$studentInfo = [];
+		for ($i=0; $i<10; $i++) {
+			// For second parameter of what to query, see http://php.net/manual/en/mongocollection.find.php
+			$statements = $statementHelper->getStatements("ayamel",[
+				'statement.actor.name' => $students[$i],
+			], [ 'statement.object.id' => true, ]
+			);
+			$count = $statements["cursor"]->count();
+
+			$studentInfo []= ["name" => $students[$i], "count" => $count];
+		}
+
+		echo json_encode($studentInfo);
+	}
+
 	// Returns content recommendations in 4 groups:
 		// Try these quiz questions (Group 1)
 		// Watch these videos before attempting these quiz questions (Group 2)
