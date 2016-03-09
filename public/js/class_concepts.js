0e13f090cff4d75b96fa57082690cbb44096658e
Isaac Pritchett <isaac.pritchett@gmail.com>
2016-03-09 16:53:33 -0700

    Wrote skeleton for class_concepts.js

---
+++ public/js/class_concepts.js
@@ -0,0 +1,22 @@
+$(function(){
+
+  d3.json("../class_stats/concepts", function(error, conceptList) {
+    var tbody = d3.select("#resultList tbody");
+    var resultListRows = tbody.selectAll("tr");
+    resultListRows = resultListRows.data(conceptList);
+      .enter()
+      .append("tr");
+
+      //Lecture number
+      resultListRows.append("td")
+        .html(function(d){ return d.id; });
+
+      //Concept Name
+      resultListRows.append("td")
+        .html(function(d) {return d.title;});
+
+      //Mastery Score
+      resultListRows.append("td")
+        .html(function(d) { return d.history[d.history.length-1]});
+  });
+});
